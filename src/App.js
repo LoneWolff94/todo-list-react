337b4cdeb48f3afe384354137420317457932a53
Nanda Kishore <nandakishoreramavath84@gmail.com>
2024-03-21 11:34:25 +0530

    todo-list-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
+
+  const addTask = () => {
+    if (taskInput.trim() === '') {
+      alert('Please enter some task to add');
+      return;
+    }
+
+    setTasks([...tasks, taskInput]);
+    setTaskInput('');
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      addTask();
+    }
+  };
+  
+  const toggleTaskCompletion = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = updatedTasks[index].startsWith('✓') ? updatedTasks[index].slice(2) : `✓ ${updatedTasks[index]}`;
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const clearAllTasks = () => {
+    setTasks([]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="header">
+          <h3>TO-DO List</h3>
+        </div>
+        <hr />
+        <div className="list">
+          <ul>
+            {tasks.map((task, index) => (
+              <li key={index} onClick={() => toggleTaskCompletion(index)} className={task.startsWith('✓') ? 'cmpltd' : ''}>
+                {task}
+                <span onClick={(e) => { e.stopPropagation(); deleteTask(index); }}>×</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="add">
+          <input 
+            type="text" 
+            value={taskInput} 
+            onChange={(e) => setTaskInput(e.target.value)} 
+            onKeyPress={handleKeyPress}
+            placeholder="task"
+          />
+          <button onClick={addTask}>add task</button>
+        </div>
+      </main>
+      {tasks.length > 0 && (
+        <div className="allClearBtn">
+          <button onClick={clearAllTasks}>clear ALL</button>
+        </div>
+      )}
     </div>
   );
 }
